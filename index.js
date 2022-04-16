635ed8f476b43385e64f4c8aa22e775fd2e52c8f
Andrea Mininni <mininniandrea@gmail.com>
2022-04-16 16:22:31 +0200

    16/04

--- index.js
+++ index.js
@@ -25,12 +25,71 @@
 ////5 --> 2 (1, 5)
 ////12 --> 6 (1, 2, 3, 4, 6, 12)
 ////30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
-function getDivisorsCnt(n){
-    let counter = 2; //// partiamo da 2 perché diamo per scontato che ogni numero è divisibile per 1 e per se stesso
-    //// il ciclo cicla fino a n / 2 perchè nessun numero è divisibile per un numero superiore alla sua metà
-    for (let index = 2; index <= n / 2; index++) {
-        if( n % index == 0) counter++;
-    }
-    return counter;
-}
-console.log(getDivisorsCnt(30))
+// function getDivisorsCnt(n){
+//     let counter = 2; //// partiamo da 2 perché diamo per scontato che ogni numero è divisibile per 1 e per se stesso
+//     //// il ciclo cicla fino a n / 2 perchè nessun numero è divisibile per un numero superiore alla sua metà
+//     for (let index = 2; index <= n / 2; index++) {
+//         if( n % index == 0) counter++;
+//     }
+//     return counter;
+// }
+// console.log(getDivisorsCnt(30))
+
+
+// function compareTriplets(a, b) {
+//     let ranks = [0,0];
+//     for(let i = 0; i <= a.length; i++){
+//         console.log(a[i], b[i])
+//         if(a[i] > b[i]){
+//             ranks[0] += 1;
+//         } else if(a[i] < b[i]){
+//             ranks[1] += 1;
+//         }
+//     }
+//     return ranks;
+// }
+// console.log(compareTriplets([1,2,3], [3,2,1]));
+
+
+////Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+// function diagonalDifference(arr) {
+//    let first = [];
+//    let second = [];
+//    for(let i = 0; i < arr.length; i++){
+//         first.push(arr[i][i])
+//         second.push(arr[i][arr[i].length - 1 - i])
+//    }
+//     return Math.abs(first.reduce((a,b) => a + b) - second.reduce((a,b) => a + b))
+// }
+   
+// let arr = [
+//     [1,2,3,],
+//     [4,5,6],
+//     [7,8,9]
+// ];
+// console.log(diagonalDifference(arr));
+
+
+////Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+// function plusMinus(arr) {
+//     let length = arr.length;
+//     let positives = 0;
+//     let negatives = 0;
+//     let zeros = 0;
+//     arr.forEach(el => {
+//         if(el > 0) positives++;
+//         if(el < 0) negatives++;
+//         if(el == 0) zeros++;
+//     })
+//     console.log(positives, negatives, zeros);
+//     positives = (positives / length).toPrecision(6);
+//     negatives = (negatives / length).toPrecision(6);
+//     zeros = (zeros / length).toPrecision(6);
+//     console.log(positives);
+//     console.log(negatives);
+//     console.log(zeros);
+
+// }
+// let arr = [-4, 3, -9, 0, 4, 1]
+// console.log(plusMinus(arr));
